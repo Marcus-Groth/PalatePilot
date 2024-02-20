ff160c99570feb1d1c48b679372610331714e71b
rodersnow <rodersnow@gmail.com>
2024-02-20 19:53:39 +0100

    RegistrationRequestDto: remove prop Roles

--- Apps/PalatePilot.Server/Dto/RegistrationRequestDto.cs
+++ Apps/PalatePilot.Server/Dto/RegistrationRequestDto.cs
@@ -12,6 +12,5 @@ namespace PalatePilot.Server.Models
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string ConfirmPassword { get; set; } = string.Empty;        
-        public string[] Roles { get; set; }
     }
 }
