0d8d4126dce4055d1a15c0387a300891a59b2ce7
rodersnow <rodersnow@gmail.com>
2024-02-20 14:21:27 +0100

    constructure injection on authService interface

--- Apps/PalatePilot.Server/Controllers/AuthController.cs
+++ Apps/PalatePilot.Server/Controllers/AuthController.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using PalatePilot.Server.Services;
+
 namespace PalatePilot.Server.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
     }
 }
