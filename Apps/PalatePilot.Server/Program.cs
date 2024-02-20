f2137fdfb86f0aabf6d6fe32a3f6cb72fb969350
rodersnow <rodersnow@gmail.com>
2024-02-20 18:58:16 +0100

    registrate Token Service

--- Apps/PalatePilot.Server/Program.cs
+++ Apps/PalatePilot.Server/Program.cs
@@ -11,10 +11,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<IAuthService, AuthService>();
+builder.Services.AddTransient<ITokenService, TokenService>();
+
+
 builder.Services.AddDbContext<AuthDbContext>(options => 
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
