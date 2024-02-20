ab7a9fbaf61e42f70047436b3d8ada925f4ef6bb
rodersnow <rodersnow@gmail.com>
2024-02-20 18:35:42 +0100

    AuthService: Add method registration These method handle: - Create new user - hashing password - save new user in the database - add role to the user

--- Apps/PalatePilot.Server/Services/AuthService.cs
+++ Apps/PalatePilot.Server/Services/AuthService.cs
@@ -11,5 +11,32 @@ namespace PalatePilot.Server.Services
             _userManger = userManager;
         }
 
+         public async Task<bool> Registration(RegistrationRequestDto request)
+        {
+           var newUser = new IdentityUser
+           {
+            UserName = request.UserName,
+            Email = request.Email
+           };
+
+            // Create a new user and hash the password
+            var result = await _userManger.CreateAsync(newUser, request.Password);
+
+            if(result.Succeeded)
+            {
+                // Add roles to this user
+                if(request.Roles.Length > 0)
+                {
+                    await _userManger.AddToRolesAsync(newUser, request.Roles);
+
+                    if(result.Succeeded)
+                    {
+                        return true;
+                    }   
+                }
+            }
+
+            return false;
+        }
     }
 }
