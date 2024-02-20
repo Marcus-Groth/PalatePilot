1470b7eee8b0c87226c85befd49616d13d26939a
rodersnow <rodersnow@gmail.com>
2024-02-20 14:11:58 +0100

    authService interface

---
+++ Apps/PalatePilot.Server/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PalatePilot.Server.Models; // Add missing import statements for LoginDto and RegistrationDto
+
+namespace PalatePilot.Server.Services
+{
+    public interface IAuthService
+    {
+    }
+}
