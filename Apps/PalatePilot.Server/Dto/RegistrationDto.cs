fa6f07edf631354c921c33c1048f2dcec6e87713
rodersnow <rodersnow@gmail.com>
2024-02-22 16:11:42 +0100

    RegistrationDto: Update RegistrationRequestDto - change 1: rename RegistrationRequestDto to RegistrationDto - change 2: add required attribute to all properties - reason 1: better name, that can be used for both request and response - reason 2: avoid invalid values in the AccountDB

--- Apps/PalatePilot.Server/Dto/RegistrationDto.cs
+++ Apps/PalatePilot.Server/Dto/RegistrationDto.cs
@@ -8,9 +8,17 @@ namespace PalatePilot.Server.Models
 {
     public class RegistrationRequestDto
     {
-        public string UserName { get; set; } = string.Empty;        
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; } = string.Empty;
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]        
         public string Email { get; set; } = string.Empty;
+        
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; } = string.Empty;
+        
+        [Required(ErrorMessage = "Confirm Password is required")]
         public string ConfirmPassword { get; set; } = string.Empty;        
     }
 }
