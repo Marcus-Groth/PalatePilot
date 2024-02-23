99cd2f43752c2d0f444254c947cd81dcf3448ab9
rodersnow <rodersnow@gmail.com>
2024-02-23 17:40:40 +0100

    Update registrationDto - change: remove ConfirmPassword property - reason: it is not needed, you could easily compare the password in the frontend

--- Apps/PalatePilot.Server/Dto/RegistrationDto.cs
+++ Apps/PalatePilot.Server/Dto/RegistrationDto.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PalatePilot.Server.Models
 {
@@ -16,9 +13,6 @@ namespace PalatePilot.Server.Models
         public string Email { get; set; } = string.Empty;
         
         [Required]
-        public string Password { get; set; } = string.Empty;
-        
-        [Required]
-        public string ConfirmPassword { get; set; } = string.Empty;        
+        public string Password { get; set; } = string.Empty;        
     }
 }
