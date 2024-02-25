4a7793d076258048a935e4d93fab7e88b2dcbfbb
rodersnow <rodersnow@gmail.com>
2024-02-25 11:29:22 +0100

    EmailService: private method CreateEmailMessage - change: a method that will create an email message from a given email - reason: email messages need to be created from the email service.

--- Apps/PalatePilot.Server/Services/EmailService/EmailService.cs
+++ Apps/PalatePilot.Server/Services/EmailService/EmailService.cs
@@ -1,12 +1,37 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using PalatePilot.Server.Configs;
+using PalatePilot.Server.Models.Dto;
 namespace PalatePilot.Server.Services.EmailService
 {
     public class EmailService
     {
-        
+        private readonly EmailConfig _emailConfig;
+        public EmailService(IOptions<EmailConfig> emailConfigOptions)
+        {
+            _emailConfig = emailConfigOptions.Value;
+        }
+
+        private MimeMessage CreateEmailMessage(EmailRequestDto request)
+        {
+            // Initialize the email message
+            var emailMessage = new MimeMessage();
+            
+            // Generate a email sender
+            MailboxAddress sender = new MailboxAddress(_emailConfig.Username, _emailConfig.Email);
+            emailMessage.From.Add(sender);
+            
+            // Generate a email receiver
+            MailboxAddress receiver = new MailboxAddress(request.Username, request.Email);
+            emailMessage.To.Add(receiver);
+
+            // Generate a email subject
+            emailMessage.Subject = request.Subject;
+
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = request.Message };
+            return emailMessage;
+        }
     }
 }
