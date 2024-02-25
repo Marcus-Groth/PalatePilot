7ee212244e20a693f25142c621699aefd3fb239a
rodersnow <rodersnow@gmail.com>
2024-02-25 11:38:30 +0100

    perform a consturction inject on IEmailService

--- Apps/PalatePilot.Server/Controllers/EmailController.cs
+++ Apps/PalatePilot.Server/Controllers/EmailController.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MailKit;
+using Microsoft.AspNetCore.Mvc;
+using PalatePilot.Server.Models;
+using PalatePilot.Server.Models.Dto;
+using PalatePilot.Server.Services.EmailService;
 
 namespace PalatePilot.Server.Controllers
 {
-    public class EmailController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
     {
-        
+        private readonly IEmailService _emailService;
+ 
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
     }
 }
