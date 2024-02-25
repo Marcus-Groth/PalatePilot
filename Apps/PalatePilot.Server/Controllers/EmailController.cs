6541c5361b2720867e3d50f76ea452868fcaf145
rodersnow <rodersnow@gmail.com>
2024-02-25 11:39:12 +0100

    EmailController: Add SendEmail action method change 1: Implement email sending functionality via SendEmail endpoint change 2: EmailRequestDto for passing email request data change 3: return Response 201 statuscode on successful email send

--- Apps/PalatePilot.Server/Controllers/EmailController.cs
+++ Apps/PalatePilot.Server/Controllers/EmailController.cs
@@ -20,5 +20,23 @@ namespace PalatePilot.Server.Controllers
         {
             _emailService = emailService;
         }
+
+        [HttpPost]
+        [Route("SendEmail")]
+        
+        public async Task<IActionResult> SendEmail(EmailRequestDto request)
+        {
+            await _emailService.SendEmailAsync(request);
+
+             // Create new response
+            var response = new SuccessResponse<object>
+            (
+                statusCode: 201,
+                title: "Created",
+                message: "Email has been sent!"
+            );
+
+            return Created(nameof(SendEmail), response);
+        }
     }
 }
