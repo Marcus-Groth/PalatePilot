1be29de1fe7ed26d87f2dc4be43be1da204d23c2
rodersnow <rodersnow@gmail.com>
2024-03-09 16:45:09 +0100

    Refactor ResetPasswordDto
    What has changed:
    - Add Datatype attribute to props: email, password and confirmPassword
    
    Reason for changes:
    - Give more details about following props

--- Apps/PalatePilot.Server/Models/Dto/ResetPasswordDto.cs
+++ Apps/PalatePilot.Server/Models/Dto/ResetPasswordDto.cs
@@ -12,13 +12,15 @@ namespace PalatePilot.Server.Models.Dto
         public string Token { get; set; } = string.Empty;
         
         [Required]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; } = string.Empty;
         
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; } = string.Empty;
 
-        [Required]
         [Compare("Password", ErrorMessage = "Password and Confirm Password do not match.")]  
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
