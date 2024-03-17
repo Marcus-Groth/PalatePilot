2119ab60300eee969f395ed8271b07934567042a
rodersnow <rodersnow@gmail.com>
2024-03-17 17:34:48 +0100

    Add empty layers Controller, Service And Repo for Food API
    What has changed:
    - Created empty FoodController class
    - Added FoodService layer
    - Implemented FoodRepository
    
    Reason for changes:
    - To establish a structured API for food item CRUD operations.
    - Separate concerns by dividing the logic across controller, service, and repository layers.

---
+++ Apps/PalatePilot.Server/Repository/IFoodRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PalatePilot.Server.Repository
+{
+    public interface IFoodRepository
+    {
+        
+    }
+}
---
+++ Apps/PalatePilot.Server/Services/FoodService/FoodService.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PalatePilot.Server.Services.FoodService
+{
+    public class FoodService : IFoodService
+    {   
+    }
+}
