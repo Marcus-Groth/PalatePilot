4648405e178129016764555b3831d55e5bbe09a2
rodersnow <rodersnow@gmail.com>
2024-03-30 14:59:46 +0100

    Update FoodCreateDto
    What has changed:
    - The Name property has been annotated with the a requirement attribute
    
    Reason for changes:
    - To ensure data integrity by requiring that every food item need to have a name.

--- Apps/PalatePilot.Server/Models/Dto/FoodDto.cs
+++ Apps/PalatePilot.Server/Models/Dto/FoodDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,7 +15,9 @@ namespace PalatePilot.Server.Models.Dto
 
     public class FoodCreateDto
     {
+        [Required]
         public string  Name { get; set; } = string.Empty;
+        
         public int Price { get; set; }
     }
 }
