d1856409c443003f8c3310bd6ee72fc8631be504
marcus groth <marcus.groth@mirror.se>
2024-05-01 14:26:23 +0200

    Implement GetById method in UserService
    What has changed:
    - Added GetById method to retrieve user information by ID
    - Throws NotFoundException if user with the specified ID is not found
    - Maps the retrieved user to a UserDto object using AutoMapper
    
    Reason for changes:
    - Providing a method to retrieve user information by ID in the UserService

--- Apps/PalatePilot.Server/Services/UserService/IUserService.cs
+++ Apps/PalatePilot.Server/Services/UserService/IUserService.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using PalatePilot.Server.Models;
+using PalatePilot.Server.Models.Domains;
+using PalatePilot.Server.Models.Dto;
 
 namespace PalatePilot.Server.Services.UserService
 {
     public interface IUserService
     {
-         Task Registration(RegistrationDto request);
+        Task Registration(RegistrationDto request);
+        Task<UserDto> GetById(string id);
     }
 }
