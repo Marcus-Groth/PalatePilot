d72b0c044d2a13de2df4ca751f9bb9d5a8425935
marcus groth <marcus.groth@mirror.se>
2024-05-06 15:46:03 +0200

    Add GetCart impl in CartRepository
    What has changed:
    - Implemented the GetCartAsync method in which retrieves a cart for a given user id
    
    Reason for changes:
    - Provide functionality to retrieve a user's cart from the database

--- Apps/PalatePilot.Server/Repository/CartRepository/ICartRepository.cs
+++ Apps/PalatePilot.Server/Repository/CartRepository/ICartRepository.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PalatePilot.Server.Models.Domains;
 
 namespace PalatePilot.Server.Repository
 {
     public interface ICartRepository
     {
-        
+        Task<Cart?> GetCartAsync(string userId);
     }
 }
