8d40205806b32ce3cbe8e01a44e7a07e89899597
marcus groth <marcus.groth@mirror.se>
2024-05-06 14:37:33 +0200

    Add CartService and ICartService
    What has changed:
    - Introduced ICartService interface and CartService class
    - ICartService defines the contract for cart-related operations in the service layer
    
    Reason for changes:
    - Setting up service layer to handle cart-related business logic and operations in the application

---
+++ Apps/PalatePilot.Server/Services/CartService/ICartService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PalatePilot.Server.Services.CartService
+{
+    public interface ICartService
+    {
+        
+    }
+}
