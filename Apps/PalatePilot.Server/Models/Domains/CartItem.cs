d167bdc0fc80d275c0137d20a2d932505585e231
marcus groth <marcus.groth@mirror.se>
2024-05-06 13:38:04 +0200

    Add Cart and CartItem domain models
    What has changed:
    - Introduced Cart and CartItem classes
    - Cart class represents a user's shopping cart with a list of cart items
    - CartItem class represents an item in the shopping cart with details like food id and quantity
    
    Reason for changes:
    - Adding domain models to represent shopping cart functionality in the application

---
+++ Apps/PalatePilot.Server/Models/Domains/CartItem.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PalatePilot.Server.Models.Domains
+{
+    // Dependent (Child)
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int FoodId { get; set; }
+        public int Quantity { get; set; }    
+
+        // Navigation properties
+        public Food Food { get; set; } = null!;
+        public virtual Cart Cart { get; set; } = null!;
+
+    }
+}
