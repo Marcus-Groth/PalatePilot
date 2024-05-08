f3a2439bad4520d497fbde29b2a4686b82db1a67
marcus groth <marcus.groth@mirror.se>
2024-05-08 13:42:45 +0200

    Add XML comments EmailDto
    What has changed:
    - Added XML comments for the Email and Username properties in the model.
    
    Reason for changes:
    - Prevent developer to write values manual everytime they restart the server

--- Apps/PalatePilot.Server/Models/Dto/EmailDto.cs
+++ Apps/PalatePilot.Server/Models/Dto/EmailDto.cs
@@ -4,11 +4,19 @@ namespace PalatePilot.Server.Models.Dto
 {
     public class EmailDto
     {
+        /// <summary>
+        /// Email
+        /// </summary>
+        /// <example>makenzie.schiller51@ethereal.email</example>
         [Required]
         [DataType(DataType.EmailAddress)]
         [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string Email { get; set; } = string.Empty;
         
+        /// <summary>
+        /// Username
+        /// </summary>
+        /// <example>Makenzie Schiller</example>
         [Required]
         public string Username { get; set; } = string.Empty;
         
