ef16c5faca6ae84a769e659be9a3bc241b8196d0
marcus groth <marcus.groth@mirror.se>
2024-05-08 13:52:33 +0200

    Add XML comments for RegistrationDto
    What has changed:
    - Added Request Body Example
    
    Reason for changes:
    - Prevent developer to customize request body in swagger UI

--- Apps/PalatePilot.Server/Models/Dto/RegistrationDto.cs
+++ Apps/PalatePilot.Server/Models/Dto/RegistrationDto.cs
@@ -5,13 +5,25 @@ namespace PalatePilot.Server.Models
 {
     public class RegistrationDto
     {
+        /// <summary>
+        /// Username
+        /// </summary>
+        /// <example>makenzie</example>
         [Required]
         public string UserName { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Email
+        /// </summary>
+        /// <example>makenzie.schiller51@ethereal.email</example>
         [Required]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; } = string.Empty;
         
+        /// <summary>
+        /// Password
+        /// </summary>
+        /// <example>Passw0rd!</example>
         [Required]
         public string Password { get; set; } = string.Empty;        
     }
