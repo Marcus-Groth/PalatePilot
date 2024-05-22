a3fbacef01fa786e4e91383c25675a22c91141c4
rodersnow <rodersnow@gmail.com>
2024-05-22 19:14:43 +0200

    commit

--- Apps/PalatePilot.Server/Models/Request/AddItemRequest.cs
+++ Apps/PalatePilot.Server/Models/Request/AddItemRequest.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace PalatePilot.Server.Models.Dto
+namespace PalatePilot.Server.Models
 {
     public class AddItemRequest
     {
