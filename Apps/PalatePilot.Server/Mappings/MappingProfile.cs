2c05cf0dbf7705d5871e88a34c5d036eb8e4d5b3
rodersnow <rodersnow@gmail.com>
2024-05-22 17:14:47 +0200

    map Name and Price property to Food.Name and Food.Price

--- Apps/PalatePilot.Server/Mappings/MappingProfile.cs
+++ Apps/PalatePilot.Server/Mappings/MappingProfile.cs
@@ -15,9 +15,13 @@ namespace PalatePilot.Server
             CreateMap<Food, FoodUpdateDto>().ReverseMap();
             CreateMap<User, UserDto>().ReverseMap();
             CreateMap<Cart, CartDto>().ReverseMap();
-            CreateMap<CartItem, CartItemDto>().ReverseMap();
+            CreateMap<CartItem, CartItemDto>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Food.Name))
+                .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Food.Price))
+                .ReverseMap();
+            
             CreateMap<Order, OrderDto>().ReverseMap();
-            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>();
             CreateMap<ShippingAddress, ShippingAddressDto>().ReverseMap();
         }
     }
