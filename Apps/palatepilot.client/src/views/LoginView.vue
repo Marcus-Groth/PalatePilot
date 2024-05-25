8058233d076d1c661c97c8b32e1a81f1de887169
rodersnow <rodersnow@gmail.com>
2024-05-25 14:27:19 +0200

    Create LoginView component
    What has changed:
    - Created a LoginView component
    - component includes a header, main content with form fields, and footer with links.
    
    Reason for changes:
    - Provide a user interface for logging in.

---
+++ Apps/palatepilot.client/src/views/LoginView.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <!-- Section -->
+  <section class="hero is-fullheight">
+    <div class="hero-body has-text-centered">
+      <div class="container">
+        <div class="columns">
+          <!-- Header -->
+          <header class="column mb-5">
+            <h1 class="title">Login today</h1>
+          </header>
+          <!-- Main content -->
+          <main class="column">
+            <form>
+              <!-- Username field -->
+              <div class="field">
+                <div class="control">
+                  <input class="input is-medium is-success" type="text" placeholder="Username">
+                </div>
+              </div>
+              <div class="field mb-5">
+                <div class="control">
+                  <!-- Password field -->
+                  <input class="input is-medium is-success" type="password" placeholder="Password">
+                </div>
+              </div>
+              <!-- Login button -->
+              <div class="field">
+                <div class="control">
+                  <button class="button is-block is-fullwidth is-medium is-success">Login</button>
+                </div>
+              </div>
+            </form>
+          </main>
+          <!-- Footer -->
+          <footer class="column">
+            <div>
+              <!-- Footer links -->
+              <div class="mb-2">
+                <a class="is-size-6" href="#">Forgot Password?</a>
+              </div>
+              <div>
+                <a class="is-size-6" href="#">Create an Account</a>
+              </div>
+            </div>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+</style>
