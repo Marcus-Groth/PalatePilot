ab4e5c3a4e21dc6f305e974d504814a07be08ccd
rodersnow <rodersnow@gmail.com>
2024-05-27 13:52:45 +0200

    Replace User with RegisterRequest in userService
    What has changed:
    - Update User type with RegisterRequest in the registration method.
    
    Reason for changes:
    - Align the registration method with the correct data structure

--- Apps/palatepilot.client/src/services/userService.ts
+++ Apps/palatepilot.client/src/services/userService.ts
@@ -1,18 +1,19 @@
+import type { RegisterRequest } from "@/requests/registerRequest";
 import axios from "axios";
-import type { User } from "@/models/user";
 
 const AUTH_URL = "https://localhost:7200/api/User";
 
 export default {
 
   // Generate new user
-  async registration(user: User): Promise<string> {
+  async registration(registerRequest: RegisterRequest): Promise<string> {
     try{
-        const url = AUTH_URL + "/Registration"
-        const response = await axios.post(url, user);
-        console.log(response.data);
-        return response.data.message;
-    }
+      const url = AUTH_URL + "/Registration"
+      const response = await axios.post(url, registerRequest);
+      console.log(response.data);
+      return response.data.message;
+    } 
+    
     catch (error: any) {
         const response = error.response;
         console.error(response.data);
