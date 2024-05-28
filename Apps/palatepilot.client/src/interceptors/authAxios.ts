8d2c7aa48d60f037789425f831dece699252825b
rodersnow <rodersnow@gmail.com>
2024-05-28 10:18:27 +0200

    Add authAxios interceptor
    What has changed:
    - Created Axios instance with a request interceptor to set the base URL.
    - and Authorization header
    
    Reason for changes:
    - Passing in jwt token in authorization header to get restrict reasources

---
+++ Apps/palatepilot.client/src/interceptors/authAxios.ts
@@ -0,0 +1,16 @@
+import localService from "@/services/localService";
+import axios from "axios";
+
+const authAxios = axios.create()
+
+authAxios.interceptors.request.use( config => {
+    config.baseURL = 'https://localhost:7200/api'
+    config.headers.Authorization = `Bearer ${localService.get('jwt')}`;
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+export default authAxios;
