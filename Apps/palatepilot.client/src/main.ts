50f930968791625a3b50e2147f06322b27ff9479
rodersnow <rodersnow@gmail.com>
2024-05-31 21:00:25 +0200

    Integrate FontAwesome icons
    What has changed:
    - Imported FontAwesome library and FontAwesomeIcon component.
    - Added the faCartShopping icon to the library.
    - Registered FontAwesomeIcon as a global component.
    
    Reason for changes:
    - Enable the use of FontAwesome icons throughout the Vue application.

--- Apps/palatepilot.client/src/main.ts
+++ Apps/palatepilot.client/src/main.ts
@@ -5,7 +5,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faCartShopping)
+
 
 const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router);
 app.mount("#app");
