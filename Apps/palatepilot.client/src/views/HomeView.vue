aec49b1501cbd9434fa36428385f02746e14fd1c
rodersnow <rodersnow@gmail.com>
2024-05-31 21:06:56 +0200

    Add handleAddButton function
    What has changed:
    - Created `handleAddButton` that will trigger addItemToCart function for each click
    
    Reason for changes:
    - Provide a clear and direct method for adding items to the cart

--- Apps/palatepilot.client/src/views/HomeView.vue
+++ Apps/palatepilot.client/src/views/HomeView.vue
@@ -2,10 +2,16 @@
 import { ref, onMounted  } from 'vue'
 import type { Food } from '@/models/food';
 import foodService from '@/services/foodService';
+import cartService from '@/services/cartService';
 
 // Define ref properties
 const pizzas = ref([] as Food[] | undefined)
 
+async function handleAddButton(foodId:number){
+  console.log("Food Id: " + foodId)
+  await cartService.addItemToCart(foodId)
+}
+  
 onMounted( async () => {
   pizzas.value = await foodService.getAll()
 });   
@@ -14,7 +20,7 @@ onMounted( async () => {
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">Pizza</h1>
+      <h1 class="title">Pizza</h1>      
       <div class="box" v-for="(pizza, index) in pizzas" :key="index">
         <div class="columns is-vcentered">
           <div class="column is-narrow">
@@ -26,22 +32,10 @@ onMounted( async () => {
           </div>
           <div class="column is-narrow">
               <p class="has-text-weight-bold">{{ pizza.price + "$" }}</p>
-              <button @click="handleAddButton" class=" mt-4 button is-success">Add</button>
+              <button @click="handleAddButton(pizza.id)" class=" mt-4 button is-success">Add</button>
           </div>
         </div>
       </div>
     </div>
   </section>
 </template>
-
-<script setup>
-import { ref } from 'vue';
-
-const pizzas = ref([
-  { number: 1, name: "Quattro Stagioni", ingredients: "Tomatsås, Ost, Skinka, Musslor, Paprika, Oliver, Champinjoner", price: "125kr" },
-  { number: 2, name: "El Paso", ingredients: "Tomatsås, Ost, Skinka, Räkor, Tonfisk, Lök", price: "125kr" },
-  { number: 3, name: "Batman", ingredients: "Tomatsås, Ost, Skinka, Räkor, Ananas, Champinjoner", price: "125kr" },
-  { number: 4, name: "Calzone Kebab, Inbakad", ingredients: "Tomatsås, Ost, Kebabkött", price: "125kr" },
-  { number: 5, name: "Calzone Classic, Inbakad", ingredients: "Tomatsås, Ost, Skinka, Köttfärs, Färsk vitlök", price: "125kr" }
-]);
-</script>
