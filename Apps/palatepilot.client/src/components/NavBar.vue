da0f2fc88a50555dc7a91173d48c8e1759ef4d7a
rodersnow <rodersnow@gmail.com>
2024-05-31 21:15:47 +0200

    Add cart icon with badge in NavBar component
    What has changed:
    - Implemented a cart icon using FontAwesome
    - Added a badge to display the number of items in the cart.
    
    Reason for changes:
    - Making it easier for users to see their cart status

--- Apps/palatepilot.client/src/components/NavBar.vue
+++ Apps/palatepilot.client/src/components/NavBar.vue
@@ -11,10 +11,46 @@ function handleLogoutButton(){
 </script>
 
 <template>
-    <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <button @click="handleLogoutButton" class="m-4  button">Logout</button>
+
+<nav class="navbar is-success is-flex is-align-items-center is-justify-content-space-between px-4 is-fixed-top" role="navigation" aria-label="main navigation">
+    <div class="navbar-start">
+        <div class="navbar-item">
+            <button @click="handleLogoutButton" class="button">Logout</button>
         </div>
-    </nav>
+    </div>
+    
+    <div class="navbar-left">
+        <div class="navbar-item">
+            <div class="cart container">
+                <font-awesome-icon class="icon  has-text-white is-medium" icon="fa-solid fa-cart-shopping" />
+                <span class="cart-badge has-background-link">4</span>
+            </div>
+        </div>
+    </div> 
+</nav>                
 </template>
+
+<style>
+
+.cart{
+    position: relative;
+}
+
+.cart-badge{
+    color: white;
+    border-radius: 50%;
+    padding: 0.05em 0.5em;
+    font-size: 0.65rem;
+    position: absolute;
+    top: -0.5em;
+    right: -0.5em;
+}
+
+
+
+
+
+
+
+</style>
   
