e72894384559375218c638e9d0be90d42535a2bb
rodersnow <rodersnow@gmail.com>
2024-06-11 16:11:40 +0200

    feat(cartStore): clear all actions, getters and states

--- Apps/palatepilot.client/src/stores/cartStore.ts
+++ Apps/palatepilot.client/src/stores/cartStore.ts
@@ -1,23 +1,8 @@
-import localService from "@/services/localService"
 import { defineStore } from "pinia"
 import { ref, watch } from "vue"
 
-export const useCartStore = defineStore('counter', () => {
-    const cartCount = ref(
-        Number(localService.get('cart_counter'))
-    )
-
-    watch(cartCount, () => {
-        localService.set('cart_counter', cartCount.value.toString())
-    });
-      
-    function increment() {
-      cartCount.value++
-    }
+export const useCartStore = defineStore('cart', () => {
     
-    function decrement(){
-        cartCount.value--;
-    }
   
-    return { cartCount, increment, decrement }
-  })
+    return {  }
+})
