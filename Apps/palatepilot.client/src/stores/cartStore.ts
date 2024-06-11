608b11e149a3fa375bcbdeafaf0bcdc9f1dbe775
rodersnow <rodersnow@gmail.com>
2024-06-11 16:26:14 +0200

    feat(cartStore): add cart state and initialize from localStorage

--- Apps/palatepilot.client/src/stores/cartStore.ts
+++ Apps/palatepilot.client/src/stores/cartStore.ts
@@ -2,6 +2,10 @@ import { defineStore } from "pinia"
 import { ref, watch } from "vue"
 
 export const useCartStore = defineStore('cart', () => {
+
+    // states
+    const cart = ref<Cart>({} as Cart);
+    const storedCart = localStorage.getItem('cart')
     
     // actions
     async function getCart(){
