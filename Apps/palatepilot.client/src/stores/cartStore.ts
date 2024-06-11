a684488044090f473de2d119359d27dea95b71b1
rodersnow <rodersnow@gmail.com>
2024-06-11 16:24:54 +0200

    feat(cartStore): action to stored cart in local storage

--- Apps/palatepilot.client/src/stores/cartStore.ts
+++ Apps/palatepilot.client/src/stores/cartStore.ts
@@ -3,6 +3,10 @@ import { ref, watch } from "vue"
 
 export const useCartStore = defineStore('cart', () => {
     
+    // actions
+    async function getCart(){
+        const result = await cartService.getCart();
+        localStorage.setItem('cart', JSON.stringify(result));
+    }
   
-    return {  }
 })
