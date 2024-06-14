b022343399410826078865b151004c410ebed55f
rodersnow <rodersnow@gmail.com>
2024-06-14 19:26:09 +0200

    feat: add food store to manage food data with Pinia

---
+++ Apps/palatepilot.client/src/stores/foodStore.ts
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import type { Food } from '@/models/food';
+import authAxios from '@/interceptors/authAxios';
+
+export const useFoodStore = defineStore('food', () => {
+  
+    // state
+    const foodList = ref<Food[]>([]);
+
+    // actions
+    async function getAll(){
+        try {
+          const response = await authAxios("/Food");
+          console.log(response.data.data);
+          foodList.value = response.data.data
+        }
+
+        catch (error: any) {
+          console.error(error.response.data);
+        }
+    }
+
+    return {foodList, getAll};
+});
