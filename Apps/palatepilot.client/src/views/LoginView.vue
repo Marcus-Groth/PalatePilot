c642fbb9b996988af8935e7a3bd1b30d992e13a7
rodersnow <rodersnow@gmail.com>
2024-06-17 20:05:18 +0200

    refactor(login): comment out cartStore usage to prevent errors
    Comment out cartStore import and usage to avoid undefined error in local storage.

--- Apps/palatepilot.client/src/views/LoginView.vue
+++ Apps/palatepilot.client/src/views/LoginView.vue
@@ -2,12 +2,12 @@
 import { computed, ref, watch } from 'vue'
 import type { LoginRequest } from '@/requests/loginRequest';
 import { useRouter }   from 'vue-router';
-import { useCartStore } from '@/stores/cartStore';
+// import { useCartStore } from '@/stores/cartStore';
 import { useAuthStore } from '@/stores/authStore';
 
 const router = useRouter();
 const authStore = useAuthStore();
-const cartStore = useCartStore()
+// const cartStore = useCartStore()
 
 const loginRequest = ref<LoginRequest>({} as LoginRequest);
 
@@ -16,7 +16,7 @@ watch( isAuthenticated, () => router.push('/'));
 
 async function handleLoginButton(){
   authStore.login(loginRequest.value);
-  cartStore.getCart()
+  // cartStore.getCart()
 }
 
 </script>
