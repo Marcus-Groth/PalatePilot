dca0d5502e5b50c7fc16546874b2b4ce22a5bd8c
rodersnow <rodersnow@gmail.com>
2024-06-20 10:05:50 +0200

    feat(): create button component

---
+++ Apps/palatepilot.client/src/components/PrimaryButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+defineProps<{
+  label: string
+}>()
+</script>
+
+<template>  
+    <button class="button is-block is-fullwidth is-medium is-primary">{{ label }}</button>
+</template>
