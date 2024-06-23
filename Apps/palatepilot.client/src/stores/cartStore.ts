f31031d7f62331e0234c23f848d05bc812ba55e6
rodersnow <rodersnow@gmail.com>
2024-06-23 15:06:29 +0200

    feature(cartStore): add addItemToCart action

--- Apps/palatepilot.client/src/stores/cartStore.ts
+++ Apps/palatepilot.client/src/stores/cartStore.ts
@@ -1,31 +1,39 @@
-import { defineStore } from "pinia"
-import { computed, ref, watch } from "vue"
+import { defineStore } from "pinia";
+import { computed, ref, watch } from "vue";
 import type { Cart } from "@/models/cart";
-import cartService from "@/services/cartService";
+import authAxios from "@/interceptors/authAxios";
+import type { ErrorResponse } from "@/models/errorResponse";
+import type { SuccessResponse } from "@/models/successResponse";
 
 export const useCartStore = defineStore('cart', () => {
     // states
     const cart = ref<Cart>({} as Cart);
     const isActive = ref(false);
-    const storedCart = localStorage.getItem('cart')
+  const storedCart = localStorage.getItem('cart');
     
-    if(storedCart != null){
+  if (storedCart != null) {
         cart.value = JSON.parse(storedCart);
     }
 
     // getters
-    const cartList = computed(() => cart.value.cartItems)
+  const cartList = computed(() => cart.value.cartItems);
     const subTotal = computed(() => cart.value.subTotal);
 
     // actions
-    async function getCart(){
-        const result = await cartService.getCart();
-        localStorage.setItem('cart', JSON.stringify(result));
+  async function addItemToCart(foodId: number) {
+    try {
+      const response = await authAxios.post(`/Cart?FoodId=${foodId}`);
+      console.log(response.data);
+    } catch (error: any) {
+      const response: ErrorResponse = error.response;
+      console.error(response.errors);
     }
+  }
 
-    function toggleModal(){
+
+  function toggleModal() {
         isActive.value = !isActive.value;
     }
 
-    return { isActive, cartList, subTotal, getCart, toggleModal }
-})
+  return { isActive, cartList, subTotal, addItemToCart, getCart, toggleModal };
+});
